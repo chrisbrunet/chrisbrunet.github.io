a91cc1639c733b40c26bc7f982f31f2e95af4556
chrisbrunet <132419309+chrisbrunet@users.noreply.github.com>
2023-07-06 15:26:44 -0600

    added raise to cards

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,21 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+// Get the card elements
+const cards = document.querySelectorAll('.card');
+
+// Add event listeners to each card
+cards.forEach((card) => {
+  // Add event listener for mouseover
+  card.addEventListener('mouseover', () => {
+    // Add a CSS class to raise the card
+    card.classList.add('raised');
+  });
+
+  // Add event listener for mouseout
+  card.addEventListener('mouseout', () => {
+    // Remove the CSS class to lower the card
+    card.classList.remove('raised');
+  });
+});
+
